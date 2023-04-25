98e3a6d6c50521611cc12873e48fa2cdd7f5f808
Anas Mahmud <admin@cloud1media.com>
2023-04-25 08:21:25 +0100

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
+
+import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx()]
+  integrations: [tailwind(), image(), mdx()],
+  adapter: vercel({
+    analytics: true,
+  }),
 });
